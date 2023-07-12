77bd23b6ac25c0bfd929be11fd6294bfb97ac49e
bigwaveBigwave <gb6ke3@naver.com>
2023-07-12 23:34:01 +0900

    +=

--- 숫자의합구하기_re/the sum of numbers.cpp
+++ 숫자의합구하기_re/the sum of numbers.cpp
@@ -11,7 +11,7 @@ int main()
     int sum = 0;
     for (int i = 0; i < numbers.length(); i++)
     {
-        sum += numbers[i] - '0'; // numbers[i]를 정수 값으로 계산하여 sum에 더하여 누적하기
+        sum = sum + numbers[i] - '0'; // numbers[i]를 정수 값으로 계산하여 sum에 더하여 누적하기
     }
     cout << sum << "\n";
 }
